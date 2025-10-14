e27b84ac5f595eeebec373a325c0f8d620766f40
Roney <guddorx@gmail.com>
2025-10-14 15:24:35 +0600

    version 1.1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/main.css'
 import './assets/_style.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
